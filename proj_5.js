3fbf5c9831b7eb6be9c71603e14f2e7f43d700c1
cameo choquer <cjchoquer@gmail.com>
2022-11-26 19:32:23 +0000

    changed nav bar + added photos

--- proj_5.js
+++ proj_5.js
@@ -1,17 +1,3 @@
-// var pic = document.getElementsByClassName('photo')
-// const photos = [...pic]
-// console.log(photos)
-
-    
-//         photos.forEach(function(photo) {
-//         photo.style.display="none";
-        
-//         slideIndex++;
-//         if(slideIndex > photos.length) {slideIndex = 1}
-//         photos[slideIndex-1].style.display = "block";
-//         setTimeout(slides,4000);
-//     });
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
