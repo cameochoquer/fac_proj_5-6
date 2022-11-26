24e0a781660139293e11cdbf2f289ba60e036578
cameo choquer <cjchoquer@gmail.com>
2022-11-26 23:08:53 +0000

    fixed button styling

--- proj_5.js
+++ proj_5.js
@@ -1,3 +1,18 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -20,3 +35,4 @@ function showSlides(n) {
 
   slides[slideIndex-1].style.display = "block";  
 };
+
